86c1a05465b224447ef563073236bb0ab9c43263
Chase Rensberger <chasehrensberger@gmail.com>
2025-11-07 21:50:21 -0500

    checkpoint

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -101,8 +101,8 @@ require("lazy").setup({
 	checker = { enabled = false },
 })
 
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 vim.api.nvim_create_autocmd("ColorScheme", {
 	pattern = "*",
