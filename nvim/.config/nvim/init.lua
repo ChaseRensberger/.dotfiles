948d1174cc3e797940b18611f6c64aecaa26d257
Chase Rensberger <chasehrensberger@gmail.com>
2026-02-06 16:45:04 -0500

    comment out opencode.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -246,26 +246,33 @@ require("lazy").setup({
 			"nosduco/remote-sshfs.nvim",
 			dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 		},
+		-- {
+		-- 	"NickvanDyke/opencode.nvim",
+		-- 	dependencies = {
+		-- 		-- Recommended for `ask()` and `select()`.
+		-- 		-- Required for `snacks` provider.
+		-- 		---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+		-- 		{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+		-- 	},
+		-- 	config = function()
+		-- 		---@type opencode.Opts
+		-- 		vim.g.opencode_opts = {}
+		-- 		vim.o.autoread = true
+		-- 		vim.keymap.set({ "n", "x" }, "<C-a>",
+		-- 			function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
+		-- 		vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end,
+		-- 			{ desc = "Add range to opencode", expr = true })
+		-- 		vim.keymap.set("n", "goo", function() return require("opencode").operator("@this ") .. "_" end,
+		-- 			{ desc = "Add line to opencode", expr = true })
+		-- 	end
+		-- },
 		{
-			"NickvanDyke/opencode.nvim",
-			dependencies = {
-				-- Recommended for `ask()` and `select()`.
-				-- Required for `snacks` provider.
-				---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
-				{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
-			},
+			"Gentleman-Programming/veil.nvim",
+			event = "VeryLazy",
 			config = function()
-				---@type opencode.Opts
-				vim.g.opencode_opts = {}
-				vim.o.autoread = true
-				vim.keymap.set({ "n", "x" }, "<C-a>",
-					function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
-				vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end,
-					{ desc = "Add range to opencode", expr = true })
-				vim.keymap.set("n", "goo", function() return require("opencode").operator("@this ") .. "_" end,
-					{ desc = "Add line to opencode", expr = true })
-			end
-		},
+				require("veil").setup()
+			end,
+		}
 		-- {
 		-- 	"mfussenegger/nvim-dap",
 		-- 	dependencies = {
@@ -291,6 +298,8 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 		end
 	end,
 })
+
+require("veil").setup()
 
 require("gitsigns").setup({
 	current_line_blame = false,
